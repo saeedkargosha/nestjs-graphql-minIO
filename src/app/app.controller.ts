56077128f39d95c39768b54027478ec74efa6aae
saeed kargosha <saeed.k@cosmopolis.co>
2023-02-14 15:41:04 +0330

    Initial commit

---
+++ src/app/app.controller.ts
@@ -0,0 +1,11 @@
+import { Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
